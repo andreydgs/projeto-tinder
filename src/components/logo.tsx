1159f539a9a72121b07382ce17af413ae41405d0
Carlos Andrey Domingues <contatoandreyd@gmail.com>
2025-09-18 17:42:40 +0000

    No lugar da logotipo, adicione essa imagem aqui https://i.imgur.com/zLYY

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,10 +1,13 @@
-import { Heart } from 'lucide-react';
+import Image from 'next/image';
 
 export function Logo() {
   return (
-    <div className="flex items-center gap-2 text-2xl font-bold font-headline text-transparent bg-clip-text bg-gradient-to-r from-[#EA6553] to-[#E34562]">
-      <Heart className="h-8 w-8 text-[#E34562]" fill="#E34562" />
-      <span className="text-3xl">Divine Match</span>
-    </div>
+    <Image
+      src="https://i.imgur.com/zLYYCsd.png"
+      alt="Divine Match Logo"
+      width={180}
+      height={45}
+      priority
+    />
   );
 }
